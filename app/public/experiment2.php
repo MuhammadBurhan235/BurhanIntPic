d2cb7a5da8c3d45b5b3eb2d000608477fd68ff48
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2024-06-30 06:38:45 +0700

    taro di ats

--- app/public/experiment2.php
+++ app/public/experiment2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kenal Al-Fath</title>
+    <link rel="stylesheet" href="experiment2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         * {
@@ -24,7 +25,6 @@
                 );
         }
     </style>
-    <link rel="stylesheet" href="experiment2.css">
 </head>
 <body>
     <div class="banner" id="banner1">
